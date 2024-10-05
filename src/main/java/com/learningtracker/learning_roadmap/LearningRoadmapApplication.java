2d525477b948780113e50250a2fdcdba4c484267
Jonatan <jonatanav255@hotmail.com>
2024-10-05 13:01:59 -0600

    format

--- src/main/java/com/learningtracker/learning_roadmap/LearningRoadmapApplication.java
+++ src/main/java/com/learningtracker/learning_roadmap/LearningRoadmapApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LearningRoadmapApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LearningRoadmapApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LearningRoadmapApplication.class, args);
+    }
 
 }
