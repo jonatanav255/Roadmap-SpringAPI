27a549ce32eebe640745df35695270557886fe2f
Jonatan <jonatanav255@hotmail.com>
2024-10-05 13:01:04 -0600

    adding user

---
+++ src/main/java/com/learningtracker/model/User.java
@@ -0,0 +1,42 @@
+package com.learningtracker.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue            
+    private int id;
+    private String username;
+    private String email;
+
+    // Getter and Setter for id
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getter and Setter for username
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    // Getter and Setter for email
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
