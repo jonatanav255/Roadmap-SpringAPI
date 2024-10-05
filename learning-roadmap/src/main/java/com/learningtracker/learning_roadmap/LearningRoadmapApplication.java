ddf748ccc174882947bd787b0f863cbe77103856
Jonatan <jonatanav255@hotmail.com>
2024-10-05 11:58:05 -0600

    basic set up

---
+++ learning-roadmap/src/main/java/com/learningtracker/learning_roadmap/LearningRoadmapApplication.java
@@ -0,0 +1,13 @@
+package com.learningtracker.learning_roadmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningRoadmapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningRoadmapApplication.class, args);
+    }
+
+}
---
+++ learning-roadmap/src/test/java/com/learningtracker/learning_roadmap/LearningRoadmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningtracker.learning_roadmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningRoadmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
